4a7c1bcc3200b4dc7a883b358a5f7afdb1f18d38
Vinh Nguyen <qvinhnguyen17@gmail.com>
2019-10-07 13:09:38 +0300

    Move 'preprocess' function to utils.py.

--- code_snippets/tf_idf.py
+++ code_snippets/tf_idf.py
@@ -3,49 +3,26 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 import os
 import gensim
-
-# Pre-process the data
-# load data
-def preprocess(start_year, end_year, companies=['']):
-    for year in range(start_year, end_year):
-        if companies[0] == '':
-            companies = os.listdir(f'forms/{str(year)}')
-        for company in companies:
-            filename = f'forms/{str(year)}/{company}'
-            with open(filename, 'r', encoding='utf-8') as f:
-                text = f.read()
-            # Remove stopwords
-            text = gensim.parsing.remove_stopwords(text)
-            # Bring to lower, strip punctuation, split by spaces
-            text = gensim.utils.simple_preprocess(text, min_len=1)
-            print(text)
-            # # filter out stop words
-            # stop_words = set(stopwords.words('english'))
-            if str(year) not in os.listdir('cleaned'):
-                os.mkdir('cleaned' + os.sep + str(year))
-            cleaned = f'cleaned/{str(year)}/{company}'
-            with open(cleaned, 'w', encoding='utf-8') as f:
-                f.write(' '.join(text))
-            # Print out the year to check missing years
-            print(year)
+from utils import *
 
 def tf_idf(start_year, end_year, companies=['']):
-    # Create an empty list for the reports
-    reports = []
+    """ Compute tf-idf scores for documents of each year
+        and return a dataframe containing 20 highest-score
+        words vs years.
+        --------------------
+        Parameter:
+            start_year: starting year of interest
+            end_year: ending year of interest
+            companies (list of str): list of interested companies
+
+        Return:
+            pd.DataFrame of tf_idf score vs years
+    """
+    # Query desired reports for tf-idf.
+    reports = query_docs(start_year, end_year, companies)
+
     # Create list of years in string format
     years = [str(year) for year in range(start_year, end_year)]
-    # Open the reports in loop
-    for year in years:
-        text_dump = ''
-        if companies[0] == '':
-            companies = os.listdir('cleaned' + os.sep + str(year))
-        # Open the report
-        for company in companies:
-            with open(f'cleaned/{year}/{company}', 'r', encoding='utf-8') as f:
-                # Read the report
-                text_dump += f.read()
-            # Append report to the list
-        reports.append(text_dump)
 
     vectorizer = TfidfVectorizer()
     vectors = vectorizer.fit_transform(reports)
