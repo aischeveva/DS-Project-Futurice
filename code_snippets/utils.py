09c22abc0039528cfaacbf41083d719912c8862f
Vinh Nguyen <qvinhnguyen17@gmail.com>
2019-10-28 09:02:00 +0200

    Add classification by industry code and convertion table.

--- code_snippets/utils.py
+++ code_snippets/utils.py
@@ -1,8 +1,13 @@
 import os
 import gensim
 import nltk
-from pathvalidate import sanitize_filename
+import re
+import requests
+import shutil
 import pandas as pd
+import lxml.html as lh
+from pathvalidate import sanitize_filename
+from bs4 import BeautifulSoup
 
 
 def rename_to_index(start_year, end_year):
@@ -39,7 +44,6 @@ def rename_to_index(start_year, end_year):
                     os.rename(old_name,
                               new_name)
 
-
 def preprocess(start_year, end_year, companies=['']):
     """ Preprocess input documents and save the results to
         folder name 'cleaned'.
@@ -61,7 +65,7 @@ def preprocess(start_year, end_year, companies=['']):
             try:
                 with open(filename, 'r', encoding='utf-8') as f:
                     text = f.read()
-            except Exception: 
+            except Exception:
                 continue
             # Remove stopwords
             text = gensim.parsing.remove_stopwords(text)
@@ -100,15 +104,96 @@ def query_docs(start_year, end_year, companies=['']):
             companies = os.listdir('cleaned' + os.sep + str(year))
         # Open the report
         for company in companies:
-            with open(f'cleaned/{year}/{company}', 'r', encoding='utf-8') as f:
-                # Read the report
-                text_dump += f.read()
-            # Append report to the list
+            try:
+                with open(f'cleaned/{year}/{company}', 'r', encoding='utf-8') as f:
+                    # Read the report
+                    text_dump += f.read()
+            except Exception:
+                continue
+        # Append report to the list
         docs.append(text_dump)
     return docs
 
+def CIK_2_SIC_series():
+    """ Get CIK <-> SIC convertion.
+        CIK is the company's ID in Edgar database.
+        SIC is the company's business sector code defined in Edgar.
+        --------------------
+        Parameter:
+            None
 
+        Return:
+            pd.Series
+    """
+    # Read CSV file.
+    df = pd.read_csv('Industry.csv', sep=';',
+            usecols=['SEC ID', 'Latest SIC Industry Code'])
+    # Rename 2 columns.
+    df = df.rename(columns={'SEC ID': 'CIK', 'Latest SIC Industry Code': 'SIC'})
+    # Filter empty cell, convert to type string and squeeze to series.
+    series = df.dropna().astype(int).astype(str).set_index('CIK').squeeze()
+    return series
 
+def SIC_2_Industry_df():
+    """ Get SIC <-> Office & Industry convertion.
+        Within an 'Office' there are several 'Industry'.
+        --------------------
+        Parameter:
+            None
 
+        Return:
+            pd.DataFrame
+    """
+    # Get url of the SIC list.
+    url='https://www.sec.gov/info/edgar/siccodes.htm'
+    # Create a handle, page, to handle the contents of the website.
+    page = requests.get(url)
+    # Store the contents of the website under doc.
+    doc = lh.fromstring(page.content)
+    # Parse data that are stored between <tr>..</tr> of HTML.
+    rows = doc.xpath('//tr')
+    # Create empty dictionary.
+    dic = {t.text_content(): [] for t in rows[0]}
+    # Read data into dictionary.
+    for row in rows[1:]:
+        data = [t.text_content() for t in row.iterchildren()]
+        dic['SIC Code'].append(data[0])
+        dic['Office'].append(data[1])
+        dic['Industry Title'].append(data[2])
+    # Return SIC dataframe.
+    return pd.DataFrame(dic).rename(columns={'SIC Code':
+        'SIC', 'Industry Title': 'Industry'}).set_index('SIC').T
 
+def classify_industry(start_year, end_year):
+    """ Classify company by industry.
+        --------------------
+        Parameter:
+            start_year: starting year of interest
+            end_year: ending year of interest
+
+        Return:
+            None
+    """
+    cik_2_sic = CIK_2_SIC_series()
+    sic_2_ind = SIC_2_Industry_df()
+
+    for year in range(start_year, end_year):
+        print(year)
+        # Get index files for this year
+        companies = os.listdir('cleaned' + os.sep + str(year))
+        for company in companies:
+            try:
+                # Get CIK, Office and Industry of the company.
+                cik = company[:-4] if '.txt' in company else company
+                office = sic_2_ind[cik_2_sic[cik]]['Office']
+                industry = sic_2_ind[cik_2_sic[cik]]['Industry']
+                # Get company file path and target industry directory.
+                old = 'cleaned' + os.sep + str(year) + os.sep + company
+                new = 'industries' + os.sep + str(year) + os.sep + office + os.sep + industry
+                # Move file.
+                if not os.path.exists(new):
+                    os.makedirs(new)
+                shutil.move(old, new)
+            except Exception:
+                continue
 
