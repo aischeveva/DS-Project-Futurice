fac5150fb683bc9761cc03b2d62a472d5467054e
Olivia Albayrak <oliviaalbayrak@Olivia-MacBook-Air.local>
2019-11-26 14:58:33 +0200

    navigation added

--- web/web_page.py
+++ web/web_page.py
@@ -4,17 +4,30 @@ from flask import request
 import re
 import pandas as pd
 import json
+import os
 
 # create Flask app
 app = Flask(__name__, static_url_path='/static')
 # open csv file
-DATA = pd.read_csv('source\\topicm_Office of Technology_Machinery & Equipment.csv', index_col=0)
-with open('source/example.txt', 'r') as infile:
+DATA = pd.read_csv('source/Office_of_Energy_& _Transportation_Mining.csv', index_col=0)
+with open('example.txt', 'r') as infile:
     TOPICS = json.load(infile)
 
 # call when main page is loaded
 @app.route('/', methods=['GET', 'POST'])
 def start_page():
+    
+
+    files = [name.split('_') for name in os.listdir('source') if name.startswith('Office')]
+    dic = {}
+    for p in files:
+        office= p[0][10:]
+        sector = p[1][:-4]
+        if office in dic:
+            dic[office].append(sector)
+        else:
+            dic[office] = [sector]
+    
     data = DATA
     if request.form:
         print(request.form['keyword'])
@@ -24,7 +37,7 @@ def start_page():
             data = data.to_frame()
     data.columns = range(1, len(data.columns)+1)
     # render template with passing data as parameter
-    return render_template('main_page.html', k=data.to_csv(), words=json.dumps(TOPICS))
+    return render_template('main_page.html', k=data.to_csv(), words=json.dumps(TOPICS), industries = dic)
 
 
 if __name__ == '__main__':
