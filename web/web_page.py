4db6a42dabe9209a7fffa29b4bc806933cd74361
aischeveva <aslonenok@gmail.com>
2019-11-27 01:04:57 +0200

    Added google interaction

--- web/web_page.py
+++ web/web_page.py
@@ -8,10 +8,6 @@ import os
 
 # create Flask app
 app = Flask(__name__, static_url_path='/static')
-# open csv file
-DATA = pd.read_csv('source/Office_of_Energy_& _Transportation_Mining.csv', index_col=0)
-with open('example.txt', 'r') as infile:
-    TOPICS = json.load(infile)
 
 # call when main page is loaded
 @app.route('/', methods=['GET', 'POST'])
@@ -28,16 +24,14 @@ def start_page():
         else:
             dic[office] = [sector]
     
-    data = DATA
-    if request.form:
-        print(request.form['keyword'])
-        word = request.form['keyword']
-        if word in DATA.columns:
-            data = DATA[word]
-            data = data.to_frame()
+    data = pd.read_csv('source/Office of Energy & Transportation_Mining.csv', index_col=0)
+    topics = []
+    for column in data.columns:
+        words = column.split(' | ')
+        topics.append([{'name': word, 'weight': len(words) + 5 - i} for i, word in enumerate(words)])
     data.columns = range(1, len(data.columns)+1)
     # render template with passing data as parameter
-    return render_template('main_page.html', k=data.to_csv(), words=json.dumps(TOPICS), industries = dic)
+    return render_template('main_page.html', k=data.to_csv(), words=json.dumps(topics), industries=dic)
 
 
 if __name__ == '__main__':
