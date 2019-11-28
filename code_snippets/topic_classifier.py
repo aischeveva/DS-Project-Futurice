6c62968841ac438746226ad0f269902619268bc8
Khoa Lai <llmkhoa511@gmail.com>
2019-11-28 16:50:07 +0200

    Fix sectors showing blank topics

--- code_snippets/topic_classifier.py
+++ code_snippets/topic_classifier.py
@@ -5,7 +5,8 @@ import os, sys
 
 def topic_classifier(): 
     #Load the model from file
-    loaded_model = joblib.load('random_forest_classifier_model.sav')
+    tight_model = joblib.load('random_forest_classifier_model.sav')
+    tolerated_model = joblib.load('random_forest_classifier_5_diff_model.sav')
     #Change the directory to the csv file storage
     os.chdir("../web/source")
     for office in os.listdir():
@@ -15,7 +16,7 @@ def topic_classifier():
            and office != 'Untitled.ipynb'):
             print('Classifying ' + office.replace(".csv",''))
             unseen_sector = pd.read_csv(office, error_bad_lines=False).drop(columns = "Unnamed: 0").T
-            predictions = loaded_model.predict(unseen_sector)
+            predictions = tight_model.predict(unseen_sector)
             #Find the lines which are unlikely to be trendy in the future
             index = []
             temp = 0
@@ -25,9 +26,30 @@ def topic_classifier():
                     temp += 1
                 else: 
                     temp += 1
+
             #Drop the line 
-            unseen_sector = unseen_sector.T.drop(unseen_sector.T.columns[[index]], axis = 1).rename(index = {0: 2010, 1 : 2011, 2: 2012, 3: 2013, 4: 2014, 5: 2015, 6: 2016, 7: 2017, 8: 2018})
-            #Save the reduced version to file 
-            unseen_sector.to_csv(os.getcwd()[:-10] + '/web/source' + os.sep + office)
+            sector = unseen_sector.T.drop(unseen_sector.T.columns[[index]], axis = 1).rename(index = {0: 2010, 1 : 2011, 2: 2012, 3: 2013, 4: 2014, 5: 2015, 6: 2016, 7: 2017, 8: 2018})
+            if (sector.shape[1] == 0):
+               unseen_sector = pd.read_csv(office, error_bad_lines=False).drop(columns = "Unnamed: 0").T
+               predictions = tolerated_model.predict(unseen_sector)
+            #Find the lines which are unlikely to be trendy in the future
+               index = []
+               temp = 0
+               for i in range(len(predictions)):
+                    if (predictions[i] == 0):
+                        index.append(temp)
+                        temp += 1
+                    else: 
+                        temp += 1
+
+               unseen_sector = unseen_sector.T.drop(unseen_sector.T.columns[[index]], axis = 1).rename(index = {0: 2010, 1 : 2011, 2: 2012, 3: 2013, 4: 2014, 5: 2015, 6: 2016, 7: 2017, 8: 2018})
+             #Save the reduced version to file 
+               unseen_sector.to_csv(os.getcwd()[:-10] + '/web/source' + os.sep + office)
+            
+            else: 
+               unseen_sector = unseen_sector.T.drop(unseen_sector.T.columns[[index]], axis = 1).rename(index = {0: 2010, 1 : 2011, 2: 2012, 3: 2013, 4: 2014, 5: 2015, 6: 2016, 7: 2017, 8: 2018})
+                 #Save the reduced version to file 
+               unseen_sector.to_csv(os.getcwd()[:-10] + '/web/source' + os.sep + office)
+
         else: 
             continue
